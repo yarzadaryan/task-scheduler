b7e78709f8daaaa0f94e111b3f8bf6d7d3cd37a7
yarzadaryan <yarzadaryan@gmail.com>
2025-10-18 11:18:16 -0400

    Updates

--- assets/Timer-CAOVXO1v.js
+++ assets/Timer-CAOVXO1v.js
@@ -1,4 +1,4 @@
-import{c as d,a as s,j as e}from"./index-DQm5r6nU.js";/**
+import{c as d,a as s,j as e}from"./index-CXBm_RPA.js";/**
  * @license lucide-react v0.546.0 - ISC
  *
  * This source code is licensed under the ISC license.
