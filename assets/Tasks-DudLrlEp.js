57343e611e77acd1a225c8186860d9b0d03a5690
yarzadaryan <yarzadaryan@gmail.com>
2025-10-18 08:44:58 -0400

    Updates

--- assets/Tasks-DudLrlEp.js
+++ assets/Tasks-DudLrlEp.js
@@ -1,4 +1,4 @@
-import{c as i,u as t,a as u,j as s}from"./index-BKjv54Nq.js";/**
+import{c as i,u as t,a as u,j as s}from"./index-Cw8QTU2b.js";/**
  * @license lucide-react v0.546.0 - ISC
  *
  * This source code is licensed under the ISC license.
